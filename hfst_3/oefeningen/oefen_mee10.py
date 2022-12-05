3efaf16f02f8626b711a318d9667c13afa246795
Rens <crre071105@leerling.mosa-rt.be>
2022-12-05 11:20:07 +0100

    dfg

--- hfst_3/oefeningen/oefen_mee10.py
+++ hfst_3/oefeningen/oefen_mee10.py
@@ -4,16 +4,17 @@ import json
  - De koningin.
  - Het aantal lopers.
 
-"""
+""" 
 def schaak_info(info):
-    for stuk, stuk_info in info.items():
-        zin = f"""Er zijn {stuk_info['aantal']} {stuk}. 
-                  De engelse naam is {stuk_info['engelse_naam']}. 
-                  Ze bewegen {stuk_info['beweging']}"""
-        print(zin)
-        
-fp = open("oefen_mee10.json", "r")
-info = json.load(fp)
-schaak_info(info)
-
-fp.close()
+        for stuk, stuk_info in info.items():
+            zin = f"""Er zijn {stuk_info['aantal']} {stuk}. 
+                    De engelse naam is {stuk_info['engelse_naam']}. 
+                    Ze bewegen {stuk_info['beweging']}"""
+            print(zin)
+try:   
+    fp = open("oefen_mee10.json", "r")
+    info = json.load(fp)
+    schaak_info(info)
+    fp.close()
+except FileNotFoundError:
+    print("Kon het opegeven bestand helaas niet vinden, neem contact met ons op. Op onderstaand nummer.")
